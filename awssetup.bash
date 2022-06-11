d07bf5927689f9a8194295680a735196514e7a77
wselby-wyo <104830658+wselby-wyo@users.noreply.github.com>
2022-06-10 23:49:51 -0600

    Create awssetup.bash

---
+++ awssetup.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt update
+apt upgrade
+apt -y install "
+git
+lynx
+memtool
+firefox
+tor
+torbrowser-launcher
+john
+build-essential
+flex
+bison
+gnucobol
+gdb
+gfortran
+nmap
+"
